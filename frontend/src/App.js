df48f2a4b6139a7287a6ee80e38e896d2f6c4991
PandelisMargaronis <pmargaro@hamilton.edu>
2024-07-10 14:52:28 -0400

    comments added / code readability improved. updates to info modal.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 import "./App.css"; // Make sure to style your components
 
 function App() {
+  // files: An array that holds the uploaded files. Initially, it's an empty array.
+  // showModal: A boolean that controls the visibility of the modal. Initially, it's false.
   const [files, setFiles] = useState([]);
   const [showModal, setShowModal] = useState(false);
-
+  /* 
+  handleFileUpload: This function is triggered when a file is uploaded. 
+  It takes the uploaded files from the event, converts them to an array, 
+  and updates the files state by adding the newly uploaded files to the existing ones.
+  */
   const handleFileUpload = (event) => {
     const uploadedFiles = Array.from(event.target.files);
     setFiles((prevFiles) => [...prevFiles, ...uploadedFiles]);
   };
-
+  /* 
+  handleRemoveFile: This function removes a file from the files 
+  array based on its index. It filters out the file at the specified 
+  index from the files array.
+  */
   const handleRemoveFile = (index) => {
     setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
   };
-
+  /* 
+  toggleModal: This function toggles the showModal state between true and false, 
+  effectively showing or hiding the modal when called.
+  */
   const toggleModal = () => {
     setShowModal(!showModal);
   };
@@ -36,9 +49,15 @@ function App() {
               &times;
             </span>
             <h2>About This Application</h2>
-            <p>This application was created by Pandelis D. Margaronis.</p>
-            <p>Use Case: To upload and query multiple PDF documents.</p>
-            <p>Steps to Use:</p>
+            <p>This application was created by Pandelis Margaronis.</p>
+            <p>
+              Use Case: To upload and query multiple PDF documents without
+              worrying about LLM hallucinations. This tool can be utilized to
+              cover reading material, prepare for exams, and identify specific
+              content within hundreds / thousands of pages, among other use
+              cases.
+            </p>
+            <p>Three Simple Steps:</p>
             <ol>
               <li>
                 Upload your PDF files using the drag and drop or browse feature.
